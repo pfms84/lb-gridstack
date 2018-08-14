056eb25ac7ecdfe3f1cfe58a432c8fe31223fdba
Paulo Santos <paulo.f.santos@glintt.com>
2018-08-14 20:49:08 +0100

    v6.0.1

--- src/app/gridstack/lib/public_api.d.ts
+++
@@ -1,5 +0,0 @@
-export { GridstackComponent } from './components/gridstack.component';
-export { GridstackItemComponent } from './components/gridstack-item.component';
-export { GridstackOptions } from './components/models/gridstack-options';
-export { Item } from './components/models/item';
-export { GridstackModule } from './gridstack.module';
